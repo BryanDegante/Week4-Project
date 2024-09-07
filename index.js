bf160ba7c7a12cedd8b7eec1f8aff58f782c0547
Bryan <bdegante0906@gmail.com>
2024-09-07 06:28:06 -0500

    stuck

---
+++ index.js
@@ -0,0 +1,33 @@
+const movieListEl = document.querySelector(".movies")
+
+async function main() {
+    const movies = await fetch("https://www.omdbapi.com/?apikey=7def6ab0&s=batman&page=4");
+    const movieData = await movies.json();
+    const movieList =  movieData.Search[1]
+    console.log(movieData)
+    console.log(movieList)
+    movieListEl.innerHTML = [movieList].map((user) => moviesHtml(user)).join("");
+}
+
+function moviesHtml(user) {
+    return `<div class="movie">
+                <figure>
+                  <img
+                    src="${user.Poster}"
+                    alt=""
+                    class="movie__poster"
+                  />
+                </figure>
+                <div class="movie__description">
+                  <h3 class="movie__title"> ${user.Title}</h3>
+                  <div class="movie__rating">
+                    <i class="fas fa-star"> 7.3</i>
+                  </div>
+                </div>
+              </div>
+    `;
+}
+
+
+
+main();
