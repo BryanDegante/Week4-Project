63397f5e3055935105c7880bcf478ff1280cea6b
Bryan <bdegante0906@gmail.com>
2024-09-13 03:53:35 -0500

    update

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const movieListEl = document.querySelector(".movies")
 
 async function main() {
-    const movies = await fetch("https://www.omdbapi.com/?apikey=7def6ab0&s=batman&page=4");
+    const movies = await fetch("https://www.omdbapi.com/?apikey=7def6ab0&s=batman&type=series");
     const movieData = await movies.json();
-    const movieList =  movieData.Search[1]
+    const movieList = movieData.Search
     console.log(movieData)
     console.log(movieList)
-    movieListEl.innerHTML = [movieList].map((user) => moviesHtml(user)).join("");
+    movieListEl.innerHTML = movieList.map((user) => moviesHtml(user)).join("");
 }
 
 function moviesHtml(user) {
@@ -21,7 +21,7 @@ function moviesHtml(user) {
                 <div class="movie__description">
                   <h3 class="movie__title"> ${user.Title}</h3>
                   <div class="movie__rating">
-                    <i class="fas fa-star"> 7.3</i>
+                    <i class="fas fa-star"> </i>
                   </div>
                 </div>
               </div>
