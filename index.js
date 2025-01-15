b18e0212cb68c41fd6ddd753e3c5ecb2c49a3675
Bryan <bdegante0906@gmail.com>
2025-01-15 13:45:53 -0600

    finsihed

--- index.js
+++ index.js
@@ -3,34 +3,34 @@ const pageListEl = document.querySelector(".page__list");
 let movieName = localStorage.getItem("movieName") || "None";
 let page = 1;
 let year = '';
+let type = "series";
 
-async function main(movieName, page, year) {
-  const movies = await fetch(`https://www.omdbapi.com/?apikey=7def6ab0&s=${movieName}&type=movie&page=${page}&y=${year}`);
+async function main(movieName, type, page, year) {
+  const movies = await fetch(`https://www.omdbapi.com/?apikey=7def6ab0&s=${movieName}&type=${type}&page=${page}&y=${year}`);
   const movieData = await movies.json();
   const movieList = movieData.Search;
   movieListEl.innerHTML = movieList.map((user) => moviesHtml(user)).join("");
   pageListEl.innerHTML = moviePages(movieData.totalResults, movieName);
 }
 
-main(movieName, page, year);
+main(movieName, type, page, year);
 
 function showUserMovies(imdbID) {
   localStorage.setItem("imdbID", imdbID);
   window.location.href = `${window.location.origin}/Week4-Project/movies.html`
-  console.log(window.location.origin);
 }
 
 async function onSearchChange(event) {
   movieName = event.target.value;
   localStorage.setItem("movieName", movieName);
-  main(movieName, page, year);
+  main(movieName, type, page, year);
 }
 
 function moviePages(totalResults, movieName) {
   let pages = Math.ceil(totalResults / 10);
   let pageList = [];
   for (let i = 1; i <= pages; i++) {
-    pageList.push(`<a class = "page__link" onclick = "main('${movieName}',${i},year)">${i}</a>`)
+    pageList.push(`<a class = "page__link" onclick = "main('${movieName}',type,${i},year)">${i}</a>`)
   }
   return pageList.join("");
 }
@@ -59,7 +59,7 @@ function movieYear() {
   var output = document.getElementById("demo");
   output.innerHTML = slider.value;
   year = (slider.value).toString();
-  main(movieName, page, year);
+  main(movieName, type, page, year);
 }
 
 function resetMovies() {
@@ -67,6 +67,26 @@ function resetMovies() {
   year = '';
   document.getElementById("slider__range").value = 1900;
   document.getElementById("demo").innerHTML = '';
-  main(movieName, page, year);
+  main(movieName, type, page, year);
 }
 
+function openMenu() {
+  document.body.classList += " filter--open"
+}
+
+function closeMenu() {
+  document.body.classList.remove('filter--open')
+}
+
+function changeType(elm) {
+  if (elm == 'movie')
+  {
+    type = elm;
+  }
+  else if (elm == 'show') {
+    type = 'series';
+  }
+
+  main(movieName, type, page, year);
+  closeMenu();
+}
